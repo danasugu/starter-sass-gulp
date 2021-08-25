a99eacdce889276de59e0a50a29d895ecfa7952a
Dana Sugu <dana.sugu@gmail.com>
2021-08-25 09:42:11 +0300

    cont.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+// const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
 
 //compile scss into css
 
-function style() {
+async function style() {
   //1. where is my scss file
-  return (
-    gulp
-      .src('./scss/**/*.scss')
-      //2. pass file through sass compiler
-      .pipe(sass())
-      //3. where do i save the compiled css?
-      .pipe(gulp.dest('./css'))
-  );
+  return await gulp
+    .src('./scss/**/*.scss')
+    //2. pass file through sass compiler
+    .pipe(sass())
+    //3. where do i save the compiled css?
+    .pipe(gulp.dest('./css'));
 }
 
 exports.style = style;
