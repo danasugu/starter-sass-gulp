7f1d4f50b88805a98f0954e785e5be97a17fe04f
Dana Sugu <dana.sugu@gmail.com>
2021-08-25 09:24:52 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+
+//compile scss into css
+
+function style() {
+  //1. where is my scss file
+  return gulp.src('./scss/**/*.scss')
+  //2. pass file through sass compiler
+  //3. where do i save the compiled css?
+
+}
